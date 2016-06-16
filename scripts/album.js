8e1eb1560a81dcfdf293a5e6853f6c1f49396f44
Tracy Royal <TracyRoyal@mac.com>
2016-06-16 10:12:43 -0400

    assignment 25 complete

--- scripts/album.js
+++ scripts/album.js
@@ -20,11 +20,27 @@ var albumMarconi = {
   year: '1909',
   albumArtUrl: 'assets/images/album_covers/20.png',
   songs: [
-    { title: 'Hello, Operator?', duration: '1:01' },
+    { title: 'Hello, Operator?', duration: '4:28' },
     { title: 'Ring, ring, ring', duration: '5:01' },
     { title: 'Fits in your pocket', duration: '3:21'},
     { title: 'Can you hear me now?', duration: '3:14' },
     { title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+
+var albumRosenberg = {
+  title: 'Live at the North Sea Jazz Festival, 1992',
+  artist: 'Rosenberg Trio',
+  label: 'EM',
+  year: '1992',
+  albumArtUrl: 'assets/images/album_covers/albumcoverTheRosenbergTrio-LiveAtTheNorthSeaJazzFestival.jpg',
+  songs: [
+    { title: 'For Sephora', duration: '1:01' },
+    { title: 'Minor Swing', duration: '2:40' },
+    { title: 'Les Yeux Noirs', duration: '2:54'},
+    { title: 'Chega de Saudade', duration: '3:37' },
+    { title: 'Bebop', duration: '2:51'},
+    { title: 'Bossa Dorado', duration: '2:40'}
   ]
 };
 
@@ -40,6 +56,13 @@ var createSongRow = function(songNumber, songName, songLength) {
 
   return template;
 };
+///assignment-25
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+///
 
 var setCurrentAlbum = function(album) {
      //select all of the HTML elements required to display on the album page
@@ -66,4 +89,14 @@ var setCurrentAlbum = function(album) {
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumRosenberg];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+       setCurrentAlbum(albums[index]);
+       index++;
+       if (index == albums.length) {
+         index = 0;
+       }
+     });
  };
